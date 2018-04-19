039d0b67c246eabe6a84f55296217b41b7aedd10
Chris Alberti <christopherjalberti@gmail.com>
2018-04-19 18:41:12 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,4 +8,11 @@ def hello_t(array)
   array
 end
 
-hello_t {}
+hello_t(["Tim", "Tom", "Jim"]) {|name| puts "Hi, #{name}" if name.start_with?("T ")}
+
+
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
